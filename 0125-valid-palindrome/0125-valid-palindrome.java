7ffa59df109081c67da32941e9d11a183234c599
Yogeesh N C <yogeeshnc832@gmail.com>
2026-03-10 22:08:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
-        while (l < r) {
-            if (!Character.isLetterOrDigit(s.charAt(l)))
-                l++;
-            else if (!Character.isLetterOrDigit(s.charAt(r)))
-                r--;
-            else if (Character.toLowerCase(s.charAt(l++)) != Character.toLowerCase(s.charAt(r--)))
-                return false;
-        }
-        return true;
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        return s.equals(new StringBuilder(s).reverse().toString());
     }
 }
