787b37f894698508aa725f41a16bb3d297b10408
Yogeesh N C <yogeeshnc832@gmail.com>
2026-03-27 20:50:44 +0530

    Time: 2 ms (90.68%), Space: 47.6 MB (91.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,7 +3,7 @@ class Solution {
         int insertPos = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // swap nums[i] with nums[insertPos]
+                
                 int temp = nums[insertPos];
                 nums[insertPos++] = nums[i];
                 nums[i] = temp;
