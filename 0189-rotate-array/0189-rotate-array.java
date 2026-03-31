12b78a5ce5842f797b9b75c5f41b63b495b04ac3
Yogeesh N C <yogeeshnc832@gmail.com>
2026-03-31 21:32:42 +0530

    Time: 0 ms (100%), Space: 61.4 MB (48.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,8 +4,9 @@ class Solution {
         k = k % n;
 
         reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
         reverse(nums, k, n - 1);
+        reverse(nums, 0, k - 1);
+        
     }
 
     public void reverse(int[] nums, int l, int r) {
